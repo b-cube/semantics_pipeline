b4b616f704c9cabd44725058a4327729b0cb3f4d
roomthily <sorenscott@gmail.com>
2015-10-11 23:32:09 -0600

    right. still. no.

--- fgdctriple_workflow.py
+++ fgdctriple_workflow.py
@@ -53,7 +53,7 @@ class FgdcParseTask(luigi.Task):
     def requires(self):
         # we have no previous task (just run from some
         # set of identified task output)
-        return None
+        return []
 
     def output(self):
         return luigi.LocalTarget(
