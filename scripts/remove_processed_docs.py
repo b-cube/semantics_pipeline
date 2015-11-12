624b6dc22c28fa194d6cc72371b5725b3fb3c3f7
roomthily <sorenscott@gmail.com>
2015-11-12 10:29:29 -0700

    nuts to that. grep ftw.

--- scripts/remove_processed_docs.py
+++ scripts/remove_processed_docs.py
@@ -1,30 +1,24 @@
 import glob
 import os
-from mpp.models import Response
+import subprocess
 
 import json as js  # name conflict with sqla
-import sqlalchemy as sqla
-from sqlalchemy.orm import sessionmaker
 
-with open('big_rds.conf', 'r') as f:
-    conf = js.loads(f.read())
-
-# our connection
-engine = sqla.create_engine(conf.get('connection'))
-Session = sessionmaker()
-Session.configure(bind=engine)
-session = Session()
-
-files = glob.glob('pipeline_tests/docs/*.json')
+files = glob.glob('../pipeline_tests/docs/*.json')
 
 for f in files:
-    fname = f.split('/').replace('.json', '')
+    fname = f.split('/')[-1].replace('.json', '')
 
-    r = session.query(Response).filter(Response.source_url_sha==fname).first()
-    if not r:
+    cmd = 'grep %s existing_shas.txt' % fname
+    s = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = s.communicate()
+
+    if err:
+        print f, err
+        continue
+
+    if not out:
         continue
 
     # delete the doc
     os.remove(f)
-
-session.close()
