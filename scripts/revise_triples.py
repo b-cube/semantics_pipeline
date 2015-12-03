d09191c053e4d03fb53183226a74a96c47bf7f1f
roomthily <sorenscott@gmail.com>
2015-12-02 19:28:43 -0700

    inject the namespace in the turtle

--- scripts/revise_triples.py
+++ scripts/revise_triples.py
@@ -5,17 +5,20 @@ import os
 # not worth regenerating through the pipeline
 # a soa local find & replace
 
-files = glob.glob('triples/*.ttl')
+files = glob.glob('../pipeline_tests/triples/*.ttl')
 
-output_dir = 'revised_triples'
+output_dir = '../pipeline_tests/revised_triples'
 
 for f in files:
     with open(f, 'r') as g:
         txt = g.read()
 
-    txt.replace(
+    txt = txt.replace(
         'bcube:reasonPhrase', 'http:reasonPhrase').replace(
         'vcard:hasUrl', 'vcard:hasURL')
+
+    # inject the namespace
+    txt = '@prefix http: <http://www.w3.org/2011/http#> .\n' + txt
     fname = f.split('/')[-1]
     with open(os.path.join(output_dir, fname), 'w') as g:
         g.write(txt)
